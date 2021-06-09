bc4b978ee9720e0a96d307833b2d8800e86a02b5
iryna-vys <vys.iryna@gmail.com>
2021-06-09 16:00:56 +0300

    Added JS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,46 @@
 'use strict';
+
+const togglerRight = document.querySelector('.toggler--right');
+const togglerLeft = document.querySelector('.toggler--left');
+
+const numbers = document.querySelectorAll('.number__item');
+const header = document.querySelector('.page__header');
+
+let count = 0;
+
+togglerRight.addEventListener('click', () => {
+  if (count < 2) {
+    count++;
+    numbers[count - 1].classList.remove('number__item--active');
+    numbers[count].classList.add('number__item--active');
+    togleBackground();
+  }
+});
+
+togglerLeft.addEventListener('click', () => {
+  if (count <= 2 && count > 0) {
+    count--;
+    numbers[count + 1].classList.remove('number__item--active');
+    numbers[count].classList.add('number__item--active');
+    togleBackground();
+  }
+});
+
+const togleBackground = () => {
+  switch (count) {
+    case 1:
+      header.classList.add('page__header--background2');
+      header.classList.remove('page__header--background3');
+      header.classList.remove('page__header--background1');
+      break;
+    case 2:
+      header.classList.add('page__header--background3');
+      header.classList.remove('page__header--background2');
+      header.classList.remove('page__header--background1');
+      break;
+    default:
+      header.classList.add('page__header--background1');
+      header.classList.remove('page__header--background3');
+      header.classList.remove('page__header--background2');
+  };
+};
